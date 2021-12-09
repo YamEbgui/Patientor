f0276d8bbe230fcbfd70d4ded9d708e11d73c94c
Yam Ebgui <yam.ebgui@gmail.com>
2021-12-10 00:34:36 +0200

    Add endpoint to add patients with all the checking that needed

--- src/controllers/patients.ts
+++ src/controllers/patients.ts
@@ -1,14 +1,28 @@
 import patients from "../patients.json";
+import { v1 as uuid } from 'uuid';
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+const id:string = JSON.stringify(uuid());
 
 type Patient = {
     "id":string,
-        "name": string,
-        "dateOfBirth": string,
-        "gender": string,
-        "occupation": string
+    "name": string,
+    "dateOfBirth": string,
+    "gender": string,
+    "occupation": string
 };
+type PatientWithSSN = {
+    id:string,
+    name: string,
+    dateOfBirth: string,
+    ssn:string,
+    gender: string,
+    occupation: string,
+};
+   
 
-export const getAllPatients = (): Patient[] => {
+type Fields = { name: unknown, dateOfBirth: unknown, gender: unknown, occupation: unknown };
+
+const getAllPatients = (): Patient[] => {
     return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
         id,
         name,
@@ -17,3 +31,46 @@ export const getAllPatients = (): Patient[] => {
         occupation
     }));
 };
+
+const isString = (text: unknown): text is string => {
+    return typeof text === 'string' || text instanceof String;
+};
+
+const isDate = (date: string): boolean => {
+    return Boolean(Date.parse(date));
+  };
+
+const parseDate = (date: unknown): string => {
+    if (!date || !isString(date)|| !isDate(date)) {
+        throw new Error(`Incorrect or missing date`);
+    }
+  
+    return date;
+};
+
+const parseString = (checkedString: unknown ,fieldName:string): string => {
+    if (!checkedString || !isString(checkedString)) {
+      throw new Error(`Incorrect or missing ${fieldName}`);
+    }
+  
+    return checkedString;
+};
+  
+const createNewPatient = ({name, dateOfBirth, gender, occupation}:Fields): PatientWithSSN => {
+    const newPatient = {
+        id: id,
+        name: parseString(name, 'name'),
+        dateOfBirth: parseDate(dateOfBirth),
+        ssn:parseString(name, 'name'),
+        gender: parseString(gender, 'gender'),
+        occupation: parseString(occupation, 'occupation')
+    };
+    return newPatient;
+};
+
+const addPatient = (object: PatientWithSSN): PatientWithSSN => {
+    patients.push(object);
+    return object;
+};
+
+export { createNewPatient , getAllPatients , addPatient };
--- src/routes/patients.ts
+++ src/routes/patients.ts
@@ -1,10 +1,29 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import express from 'express';
-import { getAllPatients } from '../controllers/patients';
+import { getAllPatients, createNewPatient , addPatient } from '../controllers/patients';
 const patientsRouter = express.Router();
 
 patientsRouter.get('/', (_req, res) => {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     res.send(getAllPatients());
+});
+
+patientsRouter.post('/', (req, res) => {
+    try {
+        console.log(req.body);
+        
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+        const newPatient = createNewPatient(req.body);
+        addPatient(newPatient);
+        res.send(newPatient);
+        
+    } catch (error: unknown) {
+        let errorMessage = 'Something went wrong.';
+        if (error instanceof Error) {
+          errorMessage += ' Error: ' + error.message;
+        }
+        res.status(400).send(errorMessage);
+    }
 });
 
 export default patientsRouter;
